533fcbf3ccc719ddbb485273c6ebe73309f9fccf
John Tillman <jct2692@gmail.com>
2019-11-26 21:00:59 -0500

    Creating Project

--- js/app.js
+++ js/app.js
@@ -1,2 +1,46 @@
 $(function () {
+  $('#repo-form').submit((event) => {
+    event.preventDefault()
+    console.log('form being submitted')
+
+    const userName = $('#github-username').val()
+    console.log(userName)
+
+    // clear list and clear text field
+    $('.repos').html('')
+    $('#github-username').val('')
+
+    search(userName)
+  })
+
+  function displayResults (repos) {
+    console.log(repos)
+    repos.forEach((repo) => {
+      $('.repos').append(
+        `<li>
+          <a href='${repo.html_url}' target='_blank'>${repo.full_name}</a>
+        </li>`
+      )
+    })
+  }
+
+  async function search (username) {
+    // ** making API request using async/await **
+    try {
+      const url = `http://www.dnd5eapi.co/api/${username}`
+
+
+      const response = await axios.get(url, {
+        params: {
+
+        }
+      })
+
+      console.log(response)
+      // displayResults(response.data)
+    } catch (error) {
+      console.log(error)
+      alert('an error occurred with your request')
+    }
+  }
 })
